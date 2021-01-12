ca7fcb13b406d6d9d6b358606beffb1c4607d903
Pranjal #120976 <120976@eesd.org>
2021-01-12 00:31:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# variables are containers that hold a value
+# Data Types/ Variables Types:
+# String (str) : combination of characters (letter, numbers, or symbols), must use quotation marks
+# Integer (int) : whole numbers
+# Character (char) : single letter, number, or symbol
+# Float : numbers with decimals
+# Boolean (bool) : True/False 
+
+# formula:
+# variableName = value
+# Rules for variable names
+# 1. Always start with a letter
+# 2. Can only contain numbers and letters
+# 3. If more than one word is used, use camelCase (first word is lowercase and every word after that is uppercase)
+
+firstName = "Pranjal" #This is a string type
+
+favoriteBook = "The Giving Tree" #string
+favNumber = 3 # 
+isHungry = True #Boolean
+favChar = 'P' #char
